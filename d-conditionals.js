5c10afb91058f327b8c0e50589b0989c4d0a3984
OzgurAbaci <o.abaci@novi-education.nl>
2020-09-25 11:50:20 +0200

    Opdracht d af

--- d-conditionals.js
+++ d-conditionals.js
@@ -1,24 +1,30 @@
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false
 // ==========================================
-
+let var1 = 10 > 20;
+console.log(var1);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true
 // ==========================================
-
+let var2 = 10 != 11;
+console.log(var2);
 
 // ==========================================
 // 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn                // geeft false
 // ==========================================
-
+let var3 = "zoet" == "sappig";
+console.log(var3);
 
 // ==========================================
 // 4a. Declareer een variabele en sla daar het rapportcijfer 9 in op.
 // 4b. schrijf een script dat checkt of het rapportcijfer groter is dan 8.
 //     Als dat zo is, loggen we 'Cum laude!' in de console.                                                     // geeft 'Cum laude!'
 // ==========================================
-
+let var4 = 9
+if (var4 > 8){
+    console.log("Cum laude!");
+}
 
 // ==========================================
 // 5a. Gebruik onderstaande variabelen isRaining en temperature;
@@ -26,3 +32,14 @@
 // 5c. Log "Doe een jas aan!" Als het regent OF kouder is dan 12 graden
 // Tip: verander de waarde van isRaining en temperature even handmatig om te kijken of jouw 2e conditie klopt
 // ==========================================
+
+let isRaining = false;
+let temperature = 10;
+
+if (!isRaining && temperature > 21){
+    console.log("Je kunt een korte broek aan!");
+}
+
+if (isRaining || temperature < 12){
+    console.log("Doe een jas aan!");
+}

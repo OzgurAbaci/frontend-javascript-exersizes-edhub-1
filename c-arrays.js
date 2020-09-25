ed9699d4a58b4e2b40310b36fd2cf32b64352748
OzgurAbaci <o.abaci@novi-education.nl>
2020-09-25 11:44:28 +0200

    opdracht c af

--- c-arrays.js
+++ c-arrays.js
@@ -1,32 +1,38 @@
 // ==========================================
 // 1. Declareer een variabele en sla daar een array met vier jaartallen in op.              // bijv. 2020, 2019, 2018 en 2017
 // ==========================================
-
+let years = [2001, 2002, 2003, 2004];
 
 // ==========================================
 // 2. Declareer een variabele en sla daar een array met drie van jouw hobbies in op.         // bijv. schaken, koekjes eten en muziek luisteren
 // ==========================================
-
+let hobbies = ["coding", "photography", "cooking"];
+console.log(hobbies);
 
 // ==========================================
 // 3a. Declareer een variabele met daarin een array met de waardes 3, 4, 5, 7 en 2
 // 3b. Log de waarde 3 uit de array in de terminal                                          // geeft 3
 // ==========================================
-
+let arr3 = [3, 4, 5, 7, 2];
+console.log(arr3[0]);
 
 // ==========================================
 // 4a. Declareer een variabele met daarin een array met de waardes groen, geel, rood, paars, blauw en oranje
 // 4b. Log de waarde blauw uit de array in de terminal                                      // geeft blauw
 // 4c. Log de waarde geel uit de array in de terminal                                       // geeft geel
 // ==========================================
-
+let arr4 = ["groen", "geel", "rood", "paars", "blauw", "oranje"];
+console.log(arr4[4]);
+console.log(arr4[1]);
 
 // ==========================================
 // 5a. Declareer een variabele met daarin een array met de waardes 21, 22, 23, 25, 25
 // 5b. Pas een van de items in de array aan zodat de cijferreeks netjes doorloopt
 // 5c. Log de de array in de terminal                                                       // geeft [ 21, 22, 23, 24, 25 ]
 // ==========================================
-
+let arr5 = [21, 22, 23, 25, 25];
+arr5[3] = 24;
+console.log(arr5);
 
 // ==========================================
 // 6a. Declareer een variabele met daarin een array met de waardes bladerdeeg, knoflook, spinazie
@@ -34,3 +40,8 @@
 // 6c. Log de de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
 // 6d. Log de lengte van de array in de terminal                                            // geeft 3
 // ==========================================
+
+let arr6 = ["bladerdeeg", "knoflook", "spinazie"];
+arr6[0] = "lasagne bladen";
+console.log(arr6);
+console.log(arr6.length);

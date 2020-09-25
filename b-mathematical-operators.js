2b0b86dc5754233a7c0e15c27a3492cc72eb49f5
OzgurAbaci <o.abaci@novi-education.nl>
2020-09-25 11:44:12 +0200

    opdracht b af

--- b-mathematical-operators.js
+++ b-mathematical-operators.js
@@ -5,38 +5,53 @@
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van 4 plus 5 in op.                           // geeft 9
 // ==========================================
-
+let var1 = 4 + 5;
+console.log(var1);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364
 // ==========================================
 
+let var2 = 7 * 52;
+console.log(var2);
 
 // ==========================================
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49
 // ==========================================
-
+let var3 = (4 + 3) * 7;
+console.log(var3);
 
 // ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12
 // ==========================================
-
+let var4 = 36 / 6 * 2;
+console.log(var4);
 
 // ==========================================
 // 5. Declareer twee variabelen en zet daarin respectievelijk de waardes 'zoet' en 'sappig'.
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig'
 // ==========================================
-
+let var5a = "zoet";
+let var5b = "sappig";
+let var5c = var5a + var5b;
+console.log(var5c);
 
 // ==========================================
 // 6. Declareer een variabele voor jouw voornaam en een variabele voor jouw achternaam en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw volledige naam en plak bovenstaande variabelen aan elkaar.
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters'
 // ==========================================
-
+let var6a = "Özgür";
+let var6b = "Abaci";
+let var6c = var6a + " " + var6b;
+console.log(var6c);
 
 // ==========================================
 // 7. Declareer een variabele voor jouw woonplaats en een variabele voor provincie en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg
 // ==========================================
+let var7a = "Oss";
+let var7b = "Noord-Brabant";
+let var7c = var7a + ", " + var7b;
+console.log(var7c);
